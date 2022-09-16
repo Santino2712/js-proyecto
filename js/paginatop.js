e1bc3f744ce70629012a14cab10f14ec5df67ec6
Santino2712 <santinogangoni271202@gmail.com>
2022-09-16 00:06:02 -0300

    proyecto entregable

---
+++ js/paginatop.js
@@ -0,0 +1,152 @@
+const baseDeDatos = [
+  {
+    id: 1,
+    nombre: "Sorrentinos",
+    precio: 1500,
+    Image: ".",
+  },
+  {
+    id: 2,
+    nombre: "Ravioles",
+    precio: 1500,
+    imagen: "ravioles.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Spaghetti",
+    precio: 1400,
+    imagen: "spaghetti.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Lasagna",
+    precio: 1600,
+    imagen: "lasagna.jpg",
+  },
+
+  {
+    id: 5,
+    nombre: "Agua",
+    precio: 200,
+    imagen: "lasagna.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Coca Cola",
+    precio: 300,
+    imagen: "lasagna.jpg",
+  },
+];
+
+let carrito = [];
+const divisa = "$";
+const DOMitems = document.querySelector("#items");
+const DOMcarrito = document.querySelector("#carrito");
+const DOMtotal = document.querySelector("#total");
+const DOMbotonVaciar = document.querySelector("#boton-vaciar");
+const DOMbotonComprar = document.querySelector("#boton-comprar");
+
+function renderizarProductos() {
+  baseDeDatos.forEach((info) => {
+    const miNodo = document.createElement("div");
+    miNodo.classList.add("card", "col-sm-4");
+
+    const miNodoCardBody = document.createElement("div");
+    miNodoCardBody.classList.add("card-body");
+
+    const miNodoTitle = document.createElement("h5");
+    miNodoTitle.classList.add("card-title");
+    miNodoTitle.textContent = info.nombre;
+
+    const miNodoImagen = document.createElement("img");
+    miNodoImagen.classList.add("img-fluid");
+    miNodoImagen.setAttribute("src", info.imagen);
+
+    const miNodoPrecio = document.createElement("p");
+    miNodoPrecio.classList.add("card-text");
+    miNodoPrecio.textContent = `${info.precio}${divisa}`;
+
+    const miNodoBoton = document.createElement("button");
+    miNodoBoton.classList.add("btn", "btn-primary");
+    miNodoBoton.textContent = "+";
+    miNodoBoton.setAttribute("marcador", info.id);
+    miNodoBoton.addEventListener("click", anyadirProductoAlCarrito);
+
+    miNodoCardBody.appendChild(miNodoImagen);
+    miNodoCardBody.appendChild(miNodoTitle);
+    miNodoCardBody.appendChild(miNodoPrecio);
+    miNodoCardBody.appendChild(miNodoBoton);
+    miNodo.appendChild(miNodoCardBody);
+    DOMitems.appendChild(miNodo);
+  });
+}
+
+function anyadirProductoAlCarrito(evento) {
+  carrito.push(evento.target.getAttribute("marcador"));
+
+  renderizarCarrito();
+}
+
+function renderizarCarrito() {
+  DOMcarrito.textContent = "";
+
+  const carritoSinDuplicados = [...new Set(carrito)];
+
+  carritoSinDuplicados.forEach((item) => {
+    const miItem = baseDeDatos.filter((itemBaseDatos) => {
+      return itemBaseDatos.id === parseInt(item);
+    });
+
+    const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+      return itemId === item ? (total += 1) : total;
+    }, 0);
+
+    const miNodo = document.createElement("li");
+    miNodo.classList.add("list-group-item", "text-right", "mx-2");
+    miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+
+    const miBoton = document.createElement("button");
+    miBoton.addEventListener("click", borrarItemCarrito);
+    miNodo.appendChild(miBoton);
+    DOMcarrito.appendChild(miNodo);
+  });
+
+  DOMtotal.textContent = calcularTotal();
+}
+
+function borrarItemCarrito(evento) {
+  const id = evento.target.dataset.item;
+
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== id;
+  });
+
+  renderizarCarrito();
+}
+
+function calcularTotal() {
+  return carrito
+    .reduce((total, item) => {
+      const miItem = baseDeDatos.filter((itemBaseDatos) => {
+        return itemBaseDatos.id === parseInt(item);
+      });
+
+      return total + miItem[0].precio;
+    }, 0)
+    .toFixed(2);
+}
+
+function vaciarCarrito() {
+  carrito = [];
+
+  renderizarCarrito();
+}
+
+function comprarCarrito() {
+  renderizarCarrito();
+}
+
+DOMbotonVaciar.addEventListener("click", vaciarCarrito);
+
+renderizarProductos();
+renderizarCarrito();
